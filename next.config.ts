d8526867857dcb34fcf64c7b4ea83daf4254ac16
Sputnik670 <ramiro.ira92@gmail.com>
2025-12-15 18:19:41 -0300

    Correccion typescript

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // @ts-expect-error: La propiedad turbopack existe en runtime pero los tipos de TS aún no la reflejan
+    turbopack: {
+      root: path.resolve(__dirname),
+    },
+  },
 };
 
 export default nextConfig;

285ea99e9b3e31740e674bb006453d07cfa39e2f
Sputnik670 <ramiro.ira92@gmail.com>
2025-12-20 20:26:15 -0300

    act vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-expect-error: La propiedad turbopack existe en runtime pero los tipos de TS aún no la reflejan
-    turbopack: {
-      root: path.resolve(__dirname),
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
